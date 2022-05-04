100b14368baea6d54cb459b1f6f136a547ccc96b
Folknology <al@folknology.com>
2022-05-05 00:06:52 +0100

    Updated Sevenseg and Tester tiles for IceLogicBus + BiNxt

--- HDL/Amaranth_Examples/Seven_Segment.py
+++ HDL/Amaranth_Examples/Seven_Segment.py
@@ -5,7 +5,7 @@ from Tiles.seven_seg_tile import SevenSegController, tile_resources
 TILE = 1
 
 
-class SevenSegExample(Elaboratable):
+class SevenSegExampleOld(Elaboratable):
     def elaborate(self, platform):
         # Add 7-segment controller
         m = Module()
@@ -35,6 +35,34 @@ class SevenSegExample(Elaboratable):
 
         return m
 
+from amaranth import *
+from mystorm_boards.icelogicbus import *
+from HDL.Amaranth_Examples.Tiles.seven_seg_tile import SevenSegController, tile_resources
+
+class SevenSegExample(Elaboratable):
+    def elaborate(self, platform):
+        m = Module()
+        m.submodules.seven = seven = SevenSegController()
+
+        seg_pins = platform.request("seven_seg_tile")
+        leds7 = Cat([seg_pins.a, seg_pins.b, seg_pins.c, seg_pins.d, seg_pins.e, seg_pins.f, seg_pins.g])
+        timer = Signal(40)
+        m.d.sync += timer.eq(timer + 1)
+
+        m.d.comb += [
+            leds7.eq(seven.leds)
+        ]
+        for i in range(3):
+            m.d.comb += seg_pins.ca[i].eq(timer[17:19] == i)
+            with m.If(seg_pins.ca[i]):
+                m.d.comb += seven.val.eq(timer[((i - 3) * 4) - 5:((i - 3) * 4) - 1])
+
+            return m
+
+def synth():
+    platform = IceLogicBusPlatform()
+    platform.add_resources(tile_resources(TILE))
+    platform.build(SevenSegExample(), do_program=True)
 
 if __name__ == "__main__":
     platform = IceLogicDeckPlatform()
--- HDL/Amaranth_Examples/Tile_tester.py
+++ HDL/Amaranth_Examples/Tile_tester.py
@@ -1,12 +1,17 @@
 from amaranth import *
 from amaranth.build import *
-from IceLogicDeck import *
+from amaranth.hdl.ast import Rose
 
+from mystorm_boards.icelogicbus import *
+from HDL.Amaranth_Examples.Tiles.seven_seg_tile import tile_resources
+
+
+TILE = 3
 
 leds12_tile = [
     Resource("leds12", 0,
 
-            Subsignal("leds", Pins("1 2 3 4 5 6 7 8 9 10 11 12", dir="o", conn=("tile", 3)), Attrs(IO_STANDARD="SB_LVCMOS")))
+            Subsignal("leds", Pins("1 2 3 4 5 6 7 8 9 10 11 12", dir="o", conn=("tile", TILE)), Attrs(IO_STANDARD="SB_LVCMOS")))
 ]
 
 
@@ -22,7 +27,7 @@ class LEDTileTest(Elaboratable):
         return m
 
 
-class QSPIE2LedTest(Elaboratable):
+class QSPIE2LedTestold(Elaboratable):
     def elaborate(self, platform):
         leds12 = Cat([l for l in platform.request("leds12")])
         qspie = Cat(platform.request("qd0").i,
@@ -38,6 +43,50 @@ class QSPIE2LedTest(Elaboratable):
 
         m.d.comb += leds12.eq(qspie)
         return m
+
+class QSPIE2LedTest(Elaboratable):
+    def elaborate(self, platform):
+        led_display = Cat([l for l in platform.request("leds12")])
+        qd0 = platform.request("qd0").i
+        qd1 = platform.request("qd1").i
+        qd2 = platform.request("qd2").i
+        qd3 = platform.request("qd3").i
+        qck = Signal()
+        qss = Signal()
+        nibble = Signal(4)
+        display = Signal(8)
+        data = Signal(8)
+
+        qdr = platform.request("qdr")
+
+        m = Module()
+
+        m.d.comb += [
+            qck.eq(platform.request("qck")),
+            qss.eq(platform.request("qss")),
+            nibble.eq(Cat(qd0, qd1, qd2, qd3)),
+            led_display.eq(Cat(display, Repl(C(0, 1), 4)))
+        ]
+
+        with m.If(qss):
+            with m.If(Rose(qss)):
+                m.d.sync += [
+                    display.eq(data),
+                    data.eq(0)
+                ]
+        with m.Else():
+            with m.If(Rose(qck)):
+                m.d.sync += [
+                    data.eq(Cat(nibble, data[:-4]))
+                ]
+
+        return m
+
+def synth():
+    platform = IceLogicBusPlatform()
+    platform.add_resources(leds12_tile)
+    # platform.build(LEDTileTest(), do_program=True)
+    platform.build(QSPIE2LedTest(), do_program=True)
 
 if __name__ == "__main__":
     platform = IceLogicDeckPlatform()
