d1db3d12bacb9eaf6fd5aad7be9857ddb26294b0
Joao Martins <joao.gumiei@gmail.com>
2022-06-29 18:14:39 +0200

    added trail example for blade1

---
+++ HDL/Examples/trail/toplevel.v
@@ -0,0 +1,27 @@
+/******************************************************************************
+*                                                                             *
+* Copyright 2016 myStorm Copyright and related                                *
+* rights are licensed under the Solderpad Hardware License, Version 0.51      *
+* (the “License”); you may not use this file except in compliance with        *
+* the License. You may obtain a copy of the License at                        *
+* http://solderpad.org/licenses/SHL-0.51. Unless required by applicable       *
+* law or agreed to in writing, software, hardware and materials               *
+* distributed under this License is distributed on an “AS IS” BASIS,          *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or             *
+* implied. See the License for the specific language governing                *
+* permissions and limitations under the License.                              *
+*                                                                             *
+******************************************************************************/
+module toplevel (
+    // 25MHz clock input
+    input  clk,
+    // use blade1 for trail
+    output [5:0] blade1
+  );
+
+  trail my_trail (
+    .clk   (clk),
+    .blade_leds (blade1[5:0])
+  );
+
+endmodule
